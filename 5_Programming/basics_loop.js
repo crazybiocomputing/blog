cda47515d9c35e804d5b48abe6395a4310089525
jeesay <jeanchristophe.taveau@gmail.com>
2021-10-20 10:58:22 +0200

    Update basics_loop.js

--- 5_Programming/basics_loop.js
+++ 5_Programming/basics_loop.js
@@ -12,6 +12,11 @@ for (let i = 0; i < 10; i++) {
 }
 
 // map(..)
-let arr=[0,1,2?3,4,5,6,7,8,9];
+let arr=[0,1,2,3,4,5,6,7,8,9];
 let output = arr.map( v => v + 2);
 console.log(output);
+
+const range(start,stop,step=1) => {
+  return Array.from( {length: Math.floor((stop - start)/step)},(_,i) => start + i*step);
+  
+console.log(range(0,10).map(v => v + 2));
