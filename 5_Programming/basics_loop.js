38b5681c6fcba779fdd72f5ef0577becafc5ef8a
jeesay <jeanchristophe.taveau@gmail.com>
2021-10-20 11:07:30 +0200

    Update basics_loop.js

--- 5_Programming/basics_loop.js
+++ 5_Programming/basics_loop.js
@@ -1,9 +1,24 @@
 
 'use strict';
 
+// Import
+const IJ_PLUGINS = IJ.getDir('plugins');
+load(`${IJ_PLUGINS}/crazybio/nashorn_polyfill.js`);
+
 // Loop while
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
 
-
+// Loop do...while
+let i = 0;
+do { 
+  console.log(i);
+  i++;
+}
+while (i < 10);
 
 // Loop for
 for (let i = 0; i < 10; i++) {
@@ -11,12 +26,13 @@ for (let i = 0; i < 10; i++) {
   console.log(j);
 }
 
-// map(..)
+// Using function map(..)
 let arr=[0,1,2,3,4,5,6,7,8,9];
 let output = arr.map( v => v + 2);
 console.log(output);
 
-const range(start,stop,step=1) => {
-  return Array.from( {length: Math.floor((stop - start)/step)},(_,i) => start + i*step);
-  
-console.log(range(0,10).map(v => v + 2));
+// Using function map(..) with a function range(..) for generating the Array
+const range = (start,stop,step=1) => Array.from( {length: Math.floor((stop - start)/step)},(_,i) => start + i*step);
+
+let output2 = range(0,10).map(v => v + 2) 
+console.log(output2);
