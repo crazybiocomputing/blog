3b48bf5a7881937c4e1b7ee5f4a4e3382150b04d
jeesay <jeanchristophe.taveau@gmail.com>
2021-10-20 10:55:38 +0200

    Update basics_loop.js

--- 5_Programming/basics_loop.js
+++ 5_Programming/basics_loop.js
@@ -1,3 +1,17 @@
+
+'use strict';
+
+// Loop while
+
+
+
+// Loop for
 for (let i = 0; i < 10; i++) {
-  console.log(i);
+  j = i + 2;
+  console.log(j);
 }
+
+// map(..)
+let arr=[0,1,2?3,4,5,6,7,8,9];
+let output = arr.map( v => v + 2);
+console.log(output);
